5bea087e87e3b8156fb24137ffd7a963c78485e8
Luis Bances <sergio5416v@gmail.com>
2026-06-06 19:31:34 -0500

    Nuevo frontend

--- clinic-frontend/src/app/pages/medicos/medicos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-medicos',
-  imports: [],
-  templateUrl: './medicos.html',
-  styleUrl: './medicos.scss',
-})
-export class Medicos {}
